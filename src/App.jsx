5ba280e684bb7f41309f697d5405ce49f137c3b0
Romie321 <raguilera321@gmail.com>
2026-09-16 00:03:02 -0700

    Render Card components in cards-grid div using cardValues array

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,10 @@ function App() {
   return (
     <div className="app">
       <GameHeader score={2} moves={3} />
-      <div className="cards-grid"></div>
+      <div className="cards-grid">
+        {cardValues.map((card)
+      <Card card={card} />)}
+      </div>
     </div>
   );
 }
