bce50ba220673ec96a771595b3fba938d3b351e4
Romie321 <raguilera321@gmail.com>
2026-09-15 23:26:44 -0700

    Add score and moves props to GameHeader component in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { GameHeader } from "./components/GameHeader";
 function App() {
   return (
     <div className="app">
-      <GameHeader />
+      <GameHeader score={} moves={}/>
     </div>
   );
 }
