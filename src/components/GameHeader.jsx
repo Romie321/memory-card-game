3c9f37d01012f809e3c8489a31e4724c4e1c560e
Romie321 <raguilera321@gmail.com>
2026-09-15 23:16:01 -0700

    Wrap stat-item and stat-name divs in a parent stat div in GameHeader component

--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -2,8 +2,10 @@ export const GameHeader = () => {
   return (
     <div className="game-header">
       <h1>Memory Card Game</h1>
-      <div className="stat-item"></div>
-      <div className="stat-name"></div>
+      <div className="stat">
+        <div className="stat-item"></div>
+        <div className="stat-name"></div>
+      </div>
     </div>
   );
 };
