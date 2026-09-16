650054d85099e443b19f428c3938640ac7a22ea3
Romie321 <raguilera321@gmail.com>
2026-09-15 23:14:53 -0700

    Add stat-item and stat-name divs to GameHeader component

--- src/components/GameHeader.jsx
+++ src/components/GameHeader.jsx
@@ -2,6 +2,8 @@ export const GameHeader = () => {
   return (
     <div className="game-header">
       <h1>Memory Card Game</h1>
+      <div className="stat-item"></div>
+      <div className="stat-name"></div>
     </div>
   );
 };
