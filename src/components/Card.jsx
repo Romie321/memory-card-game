45840619126f8e3b8ef66143ce1204486aea8f26
Romie321 <raguilera321@gmail.com>
2026-09-18 18:59:34 -0700

    Add card front component to display placeholder

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 export const card = ({ card }) => {
   return (
     <div className="card">
+      <div className="card-front">?</div>
       <div className="card-back">{card}</div>
     </div>
   );
