287280c321fa5433b440a68aa4ba65c85c3e00e1
Romie321 <raguilera321@gmail.com>
2026-09-19 20:39:18 -0700

    Fix onClick handler in card component to pass card data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export const card = ({ card, onClick }) => {
   return (
-    <div className="card" onClick={() => onClick()}>
+    <div className="card" onClick={() => onClick(card)}>
       <div className="card-front">?</div>
       <div className="card-back">{card.value}</div>
     </div>
