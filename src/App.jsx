628035efbb3cb637c4a39780720182fb44be2383
Romie321 <raguilera321@gmail.com>
2026-09-20 22:21:42 -0700

    Implement match checking logic for flipped cards in handleCardClick

--- src/App.jsx
+++ src/App.jsx
@@ -65,6 +65,9 @@ function App() {
     // Check for match when two cards are flipped
     if (flippedCards.length === 1) {
       const firstCard = cards[flippedCards[0]];
+
+      if (firstCard.value === card.value) {
+      }
     }
   };
 
