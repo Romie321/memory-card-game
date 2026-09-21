897a67435eefe58015b8fb0a3fe8ec8059d2cad0
Romie321 <raguilera321@gmail.com>
2026-09-20 22:00:15 -0700

    Fix syntax error in card click handler and add state for flipped cards

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ const cardValues = [
 
 function App() {
   const [cards, setCards] = useState([]);
+  const [flippedCards, setFlippedCards] = useState([])
 
   const initializeGame = () => {
     //Shuffle cards
@@ -45,7 +46,7 @@ function App() {
     // If card is already flipped and/or matched then you can't click again.
     if (card.isFlipped) || (card.isMatched) {
       return;
-    }
+    };
 
     // Update card flipped state.
     const newCards = cards.map((c) => {
