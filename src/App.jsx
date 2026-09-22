b70bb04d4054087750f788d51eca45e0aa7051b8
Romie321 <raguilera321@gmail.com>
2026-09-21 23:54:21 -0700

    Fix typo in setMatchedCards function call

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ function App() {
       const firstCard = cards[flippedCards[0]];
 
       if (firstCard.value === card.value) {
-        sstMatchedCards((prev) => [...prev, firstCard.id, card.id]);
+        setMatchedCards((prev) => [...prev, firstCard.id, card.id]);
         const newMatchedCards = cards.map((c) => {
           if (c.id === card.id || c.id === firstCard.id) {
             return { ...c, isMatched: true };
