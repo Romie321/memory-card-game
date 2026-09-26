130d89623646ede4147cc351663fc914060e6f2f
Romie321 <raguilera321@gmail.com>
2026-09-26 00:05:20 -0700

    Fix shuffleArray function to return shuffled array

--- src/App.jsx
+++ src/App.jsx
@@ -35,6 +35,7 @@ function App() {
       const j = Math.floor(Math.random() * (i + 1));
       [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
     }
+    return shuffled;
   };
 
   const initializeGame = () => {
