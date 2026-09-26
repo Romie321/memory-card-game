0e1d1bf1f7780fc6de6e58fedc6ad4ad87a21eee
Romie321 <raguilera321@gmail.com>
2026-09-26 00:01:21 -0700

    Implement shuffle logic in shuffleArray function

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ function App() {
 
   const shuffleArray = (array) => {
     const shuffled = [...array];
+    for (let i = shuffled.length -1; i > 0; i--)
   };
 
   const initializeGame = () => {
